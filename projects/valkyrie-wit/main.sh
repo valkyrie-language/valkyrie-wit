712bef9efa66ecc8225e8feaff5c668eda0800dc
Aster <aster@vers.site>
2023-10-27 22:44:03 +0800

    Add build script

---
+++ projects/valkyrie-wit/main.sh
@@ -0,0 +1,2 @@
+cargo build --target wasm32-wasi
+wasm-tools component new ./target/wasm32-wasi/debug/valkyrie_wit.wasm -o valkyrie-core.wasm --adapt ./wasi_snapshot_preview1.wasm
--- projects/valkyrie-wit/src/lib.rs
+++ projects/valkyrie-wit/src/lib.rs
@@ -4,6 +4,26 @@
 #![doc(html_logo_url = "https://raw.githubusercontent.com/oovm/shape-rs/dev/projects/images/Trapezohedron.svg")]
 #![doc(html_favicon_url = "https://raw.githubusercontent.com/oovm/shape-rs/dev/projects/images/Trapezohedron.svg")]
 
-mod errors;
+wit_bindgen::generate!({
+    // the name of the world in the `*.wit` input file
+    world: "host",
 
-pub use crate::errors::{Error, Result};
+    // For all exported worlds, interfaces, and resources, this specifies what
+    // type they're corresponding to in this module. In this case the `MyHost`
+    // struct defined below is going to define the exports of the `world`,
+    // namely the `run` function.
+    exports: {
+        world: MyHost,
+    },
+});
+
+// Define a custom type and implement the generated `Guest` trait for it which
+// represents implementing all the necessary exported interfaces for this
+// component.
+struct MyHost;
+
+impl Guest for MyHost {
+    fn run() {
+        print("Hello, world!");
+    }
+}
