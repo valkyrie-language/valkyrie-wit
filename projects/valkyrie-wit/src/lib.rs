7326474eb7005356a0c3eb69c25d44d2437e4b17
Aster <aster@vers.site>
2023-11-02 16:43:57 +0800

    Continue to organize the definition of primitive types

--- projects/valkyrie-wit/src/lib.rs
+++ projects/valkyrie-wit/src/lib.rs
@@ -6,6 +6,18 @@
 
 
 mod primitive;
+pub use crate::primitive::{NaturalHost, DecimalHost, IntegerHost};
 
+wit_bindgen::generate!({
+    world: "ffi",
 
-
+    // For all exported worlds, interfaces, and resources, this specifies what
+    // type they're corresponding to in this module. In this case the `MyHost`
+    // struct defined below is going to define the exports of the `world`,
+    // namely the `run` function.
+    exports: {
+        "vit:core/natural/natural": NaturalHost,
+        "vit:core/decimal": DecimalHost,
+        "vit:core/integer": IntegerHost,
+    },
+});
--- projects/valkyrie-wit/src/primitive/mod.rs
+++ projects/valkyrie-wit/src/primitive/mod.rs
@@ -1,1 +1,47 @@
-mod integer;
+use crate::exports::vit::core::{decimal, integer};
+use crate::exports::vit::core::decimal::Decimal;
+use crate::exports::vit::core::integer::Integer;
+use crate::exports::vit::core::natural::{GuestNatural, OwnNatural};
+
+
+pub struct NaturalHost;
+
+
+
+impl GuestNatural for NaturalHost {
+    fn new(init: Vec<u32>) -> Self {
+        todo!()
+    }
+
+    fn add_u32(&self, rhs: u32) -> OwnNatural {
+        todo!()
+    }
+
+    fn add_u64(&self, rhs: u64) -> OwnNatural {
+        todo!()
+    }
+
+    fn add_nat(&self, rhs: &NaturalHost) -> OwnNatural {
+        todo!()
+    }
+}
+
+
+pub struct IntegerHost;
+impl integer::Guest for IntegerHost {
+    fn add_u32(self_: Integer, rhs: u32) -> Integer {
+        todo!()
+    }
+}
+
+pub struct DecimalHost;
+
+impl decimal::Guest for IntegerHost {
+    fn add_u32(rhs: u32) -> Decimal {
+        todo!()
+    }
+
+    fn add_u64(rhs: u64) -> Decimal {
+        todo!()
+    }
+}
