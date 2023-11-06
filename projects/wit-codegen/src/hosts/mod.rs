bfb198052c7ef328241c8dd45ed504aea8903994
Aster <aster@vers.site>
2023-11-06 13:59:18 +0800

    Manually generate rust binding code

--- projects/wit-codegen/src/hosts/mod.rs
+++ projects/wit-codegen/src/hosts/mod.rs
@@ -1,40 +1,63 @@
-use std::path::{Path, PathBuf};
-use wit_bindgen_core::{
-    wit_parser::{PackageId, Resolve, UnresolvedPackage},
-    Files, Source,
+use std::{
+    collections::HashMap,
+    fs::File,
+    io::Write,
+    path::{Path, PathBuf},
 };
-use wit_bindgen_rust::Opts;
+use wit_bindgen_core::{
+    wit_parser::{PackageId, Resolve, World},
+    Files,
+};
+use wit_bindgen_rust::{ExportKey, Opts};
 
 #[test]
 fn test() {
-    parse_source();
+    let _ = parse_source();
 }
 
 fn parse_source() -> anyhow::Result<(Resolve, PackageId)> {
-    let mut a = Opts::default().build();
     let mut resolve = Resolve::default();
     // let mut files = Vec::new();
-    let root = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
-    let (pkg, filebuf) = resolve.push_dir(&root.join("wit"))?;
-    let mut files = Files::default();
-    for i in filebuf {
-        println!("Found: {:?}", i);
-        files.push(&i.as_os_str().to_string_lossy(), i.as_os_str().as_encoded_bytes())
-    }
+    let here = Path::new(env!("CARGO_MANIFEST_DIR")).canonicalize()?;
+
+    let (pkg, _) = resolve.push_dir(&here.join("wit"))?;
     //
     for (_, item) in &resolve.packages {
         println!("{:?}", item.name);
     }
+    resolve.worlds.alloc(World {
+        name: "text-ffi".to_string(),
+        imports: Default::default(),
+        exports: Default::default(),
+        package: None,
+        docs: Default::default(),
+        includes: vec![],
+        include_names: vec![],
+    });
     //
     for (_, item) in &resolve.worlds {
-        println!("{:?}", item.name);
+        println!("Worlds: {:?}", item.name);
     }
 
     //
-    let world = resolve.select_world(pkg, Some("image-ffi"))?;
-    println!("{:?}", world);
-    a.generate(&resolve, world, &mut files)?;
-    a.finish(&resolve, world, &mut files);
+    let mut exports: HashMap<ExportKey, String> = Default::default();
+    exports.insert(ExportKey::Name("vit:number/natural/natural".to_string()), "Host1".to_string());
+    exports.insert(ExportKey::Name("vit:number/integer".to_string()), "Host2".to_string());
+    exports.insert(ExportKey::Name("vit:number/integer".to_string()), "Host3".to_string());
+    exports.insert(ExportKey::Name("vit:number/ordinal".to_string()), "Host4".to_string());
+    exports.insert(ExportKey::Name("vit:number/decimal".to_string()), "Host5".to_string());
+    exports.insert(ExportKey::Name("vit:number/fraction".to_string()), "Host6".to_string());
+    exports.insert(ExportKey::Name("vit:number/prime".to_string()), "Host7".to_string());
+
+    let mut builder = Opts { rustfmt: false, exports, ..Default::default() }.build();
+    let mut files = Files::default();
+    let world = resolve.select_world(pkg, Some("number-ffi"))?;
+    builder.generate(&resolve, world, &mut files).unwrap();
+
+    for (name, content) in files.iter() {
+        let mut file = File::create(here.join("tests").join("codegen").join(name))?;
+        file.write_all(content)?;
+    }
 
     Ok((resolve, pkg))
 }
--- projects/wit-codegen/tests/main.rs
+++ projects/wit-codegen/tests/main.rs
@@ -1,3 +1,4 @@
+mod codegen;
 #[test]
 fn ready() {
     println!("it works!")
