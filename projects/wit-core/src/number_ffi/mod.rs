c0eb23bc701459a9b5cfb6669b5347920a779694
Aster <aster@vers.site>
2024-05-15 18:58:50 +0800

    Load foreign class and enumerates

--- projects/wit-core/src/number_ffi/mod.rs
+++
@@ -1,8 +0,0 @@
-use crate::exports::v::core::number::{Guest, Integer, Natural};
-pub mod decimal;
-pub mod fraction;
-pub mod integer;
-pub mod natural;
-pub mod ordinal;
-pub mod prime;
-pub mod sign;
